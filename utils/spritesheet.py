31d24cdb5f8c659d2d3a6e443f85fc7df3929cfa
Axel Sundqvist <120595757+AxelSuu@users.noreply.github.com>
2025-03-02 20:11:08 +0100

    Update spritesheet.py

--- utils/spritesheet.py
+++ utils/spritesheet.py
@@ -1,17 +1,25 @@
 import pygame as pg
+import json
 import os
-
-# This file was not implemented, boilerplate code for Spritesheet class
+    
 
 class Spritesheet:
     def __init__(self, filename):
-        self.filename = filename
-        img_folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "img"))
-        self.sprite_sheet = pg.image.load(os.path.join(img_folder_path, filename)).convert_alpha()
+        self.img_folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "imgs"))
+        self.filename = os.path.join(self.img_folder_path, filename)
+        self.sprite_sheet = pg.image.load(self.filename).convert_alpha()
+        self.meta_data = self.filename.replace('png', 'json')
+        with open(self.meta_data) as f:
+            self.data = json.load(f)
+        f.close()
 
-
-    def get_sprite(self, x, y, w, h):
-        sprite = pg.Surface((w, h))
-        sprite.set_colorkey((0,0,0))
-        sprite.blit(self.sprite_sheet,(0, 0),(x, y, w, h))
+    def get_sprite(self, x, y, width, height):
+        sprite = pg.Surface((width, height), pg.SRCALPHA)
+        sprite.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
         return sprite
+    
+    def parse_sprite(self, name):
+        sprite = self.data['sprites'][name]
+        x, y, w, h = sprite['x'], sprite['y'], sprite['width'], sprite['height']
+        image = self.get_sprite(x, y, w, h)
+        return image
